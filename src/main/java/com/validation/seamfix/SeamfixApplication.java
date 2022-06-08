8b69e1016d1e1f6d09c1cc94c705db30f4890f96
Nonso01 <chinonso.edozie@ubagroup.com>
2022-06-08 17:23:36 +0100

    initial commit

---
+++ src/main/java/com/validation/seamfix/SeamfixApplication.java
@@ -0,0 +1,13 @@
+package com.validation.seamfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeamfixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeamfixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/validation/seamfix/SeamfixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.validation.seamfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeamfixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
